327f08d3ed25cd84fb698af83e7a5020adeebc95
Bajrang-DS <bajrang.goswami@dotsquares.com>
2023-02-13 12:08:29 +0530

    kg changed

--- src/components/layouts/BreadCrumbs.tsx
+++ src/components/layouts/BreadCrumbs.tsx
@@ -1,97 +1,126 @@
 import * as React from "react";
+import { breadcrumbhome, conversionDetailsDirection } from "../../../site-global/global";
+import { regionNames } from "../../../site-global/global";
+import { Link } from "@yext/pages/components";
 type data = {
   name: any;
   parents: any;
   baseUrl: any;
   address: any;
-  slug: any;
 };
 
 const BreadCrumbs = (props: data) => {
+
   const [list, setList] = React.useState(null);
-  var breadcrumbs;
+ var breadcrumbs;
   var data: any = [];
   React.useEffect(() => {
     setURL(props.parents, props.baseUrl);
+  console.log(props.parents)
   }, [setList]);
 
   const setURL = (parents: any, baseUrl: any) => {
+
+
     if (parents) {
       for (let i = 0; i < parents.length; i++) {
-        if (parents[i].meta.entityType.id == "ce_country") {
-          parents[i].name = parents[i].name;
+
+           
+       if (parents[i].meta.entityType.id == "ce_country") {
+          // parents[i].name = regionNames.of(parents[i].name);
+ 
           parents[i].slug = parents[i].slug;
+          
           data.push({
-            name: parents[i].name,
+            name: regionNames.of(parents[i].name),
             slug: parents[i].slug,
+            count:parents[i].dm_directoryChildrenCount
           });
-        } else if (parents[i].meta.entityType.id == "ce_region") {
-          data.push({ name: parents[i].name, slug: parents[i].slug });
+
+        } 
+        else if (parents[i].meta.entityType.id == "ce_region") {
+      
+          data.push({ name: parents[i].name, slug:`${parents[i-1].slug}/${parents[i].slug}`, 
+          count:parents[i].dm_directoryChildrenCount});
           parents[i].name = parents[i].name;
-          parents[i].slug = `${parents[i].slug}`;
+          parents[i].slug = `${parents[i-1].slug}/${parents[i].slug}`;
         } else if (parents[i].meta.entityType.id == "ce_city") {
+       
           parents[i].name = parents[i].name;
-          parents[i].slug = `${parents[i].slug}`;
+          parents[i].slug = `${parents[i - 1].slug}/${parents[i].slug}`;
           data.push({
             name: parents[i].name,
             slug: parents[i].slug,
+            count:parents[i].dm_directoryChildrenCount
           });
         }
       }
 
+
       breadcrumbs = data.map((crumb: any) => (
         <li key={crumb.slug}>
-          <a href={baseUrl +  crumb.slug + ".html"}>{crumb.name}</a>
-        </li>
+          {(crumb.count==0)?<Link href="javascript:void(0)" className="cursor-not-allowed"
+          data-ya-track="Breadcrumbs"
+          eventName={`Breadcrumbs`}
+          rel="noopener noreferrer"
+          conversionDetails={conversionDetailsDirection}
+          > {crumb.name}</Link>
+          :<Link href={baseUrl + crumb.slug + ".html"}
+          data-ya-track="Breadcrumbs"
+          eventName={`Breadcrumbs`}
+          rel="noopener noreferrer"
+          conversionDetails={conversionDetailsDirection}> {crumb.name}</Link>}
+          
+        </li> 
       ));
       setList(breadcrumbs);
     } else {
       setList(null);
     }
   };
-
   return (
     <div className="breadcrumb">
-      <div className="container">
-        <ul>
+      <div className="container mx-auto">
+        <ul className="flex">
           <li>
-            <a href="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="19.668"
-                height="18.155"
-                viewBox="0 0 19.668 18.155"
-              >
-                <path
-                  d="M10.94,22.655V16.6h4.539v6.052h4.614V13.577h2.95L13.209,4.5,3.375,13.577h2.95v9.077Z"
-                  transform="translate(-3.375 -4.5)"
-                  fill="DarkOrange"
-                />
-              </svg>
-            </a>
+            <Link className="home" href="/"
+             data-ya-track="Breadcrumbs"
+             eventName={`Breadcrumbs`}
+             rel="noopener noreferrer"
+             conversionDetails={conversionDetailsDirection}>
+            <div dangerouslySetInnerHTML={{__html: breadcrumbhome}}/>
+            </Link>
           </li>
-
+          {/* <li>
+            <a href="https://main-sushi--issue--quotation-sbx-pgsdemo-com.sbx.preview.pagescdn.com/">Store Locator</a>
+          </li> */}
           {list ? (
             list
           ) : (
             <>
               {props.address && props.address.city ? (
-                <li>
+                <li className="inline-block">
                   {" "}
-                <a href={props.baseUrl + props.address.city + ".html"}>
-                {props.address.city ? props.address.city : ""}
-                  </a>
+                  <Link href={props.baseUrl + props.address.city }
+                   data-ya-track="Breadcrumbs"
+                   eventName={`Breadcrumbs`}
+                   rel="noopener noreferrer"
+                   conversionDetails={conversionDetailsDirection}>
+                    {props.address.city ? props.address.city : ""}
+                  </Link>
                 </li>
               ) : (
                 <></>
               )}
             </>
           )}
-           <li>{props && props.name}</li>
+         
+          <li>{props && props.name}</li>
+
         </ul>
       </div>
     </div>
+       
   );
 };
-
 export default BreadCrumbs;
